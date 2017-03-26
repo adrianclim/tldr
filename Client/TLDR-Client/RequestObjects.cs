196b8bbeb88758c21521e324a13aa0c474934784
Thai Quoc Nguyen <thai_ng@outlook.com>
2017-03-25 22:23:22 -0700

    serialize request object deserialize response object

---
+++ Client/TLDR-Client/RequestObjects.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TLDR_Client
+{
+    public class RequestObject
+    {
+        public string content { get; set; }
+    }
+
+    public class ResponseObject
+    {
+        public string summary { get; set; }
+    }
+}
--- Client/TLDR-Client/TextPage.xaml.cs
+++ Client/TLDR-Client/TextPage.xaml.cs
@@ -2,6 +2,7 @@
 using Windows.UI.Xaml.Controls;
 using Windows.Web.Http;
 using System;
+using Newtonsoft.Json;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -24,12 +25,19 @@ namespace TLDR_Client
 
             // Do the thing
             var client = new HttpClient();
-            var messageContent = new HttpStringContent(OriginalText.Text);
+            var requestObject = new RequestObject() { content = OriginalText.Text };
+            var requestObjectString = JsonConvert.SerializeObject(requestObject);
+            var messageContent = new HttpStringContent(requestObjectString);
             var message = new HttpRequestMessage(HttpMethod.Post, new System.Uri("127.0.0.1"))
             {
                 Content = messageContent
             };
-            await client.SendRequestAsync(message);
+
+            var response = await client.SendRequestAsync(message);
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            var responseObject = JsonConvert.DeserializeObject<ResponseObject>(responseString);
+            SummarizedText.Text = responseObject.summary;
 
             SummarizedText.IsEnabled = true;
             ProgressBar.Visibility = Visibility.Collapsed;
