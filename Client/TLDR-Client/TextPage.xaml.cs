45a950020a24fce9a3b1f43f34fe153e07cf1971
Thai Quoc Nguyen <thai_ng@outlook.com>
2017-03-25 19:20:02 -0700

    TextPage do the thing

--- Client/TLDR-Client/TextPage.xaml.cs
+++ Client/TLDR-Client/TextPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.Web.Http;
+using System;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -15,11 +17,19 @@ namespace TLDR_Client
             this.InitializeComponent();
         }
 
-        private void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        private async void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
             ProgressBar.Visibility = Visibility.Visible;
             SummarizedText.IsEnabled = false;
+
             // Do the thing
+            var client = new HttpClient();
+            var messageContent = new HttpStringContent(OriginalText.Text);
+            var message = new HttpRequestMessage(HttpMethod.Post, new System.Uri("127.0.0.1"))
+            {
+                Content = messageContent
+            };
+            await client.SendRequestAsync(message);
 
             SummarizedText.IsEnabled = true;
             ProgressBar.Visibility = Visibility.Collapsed;
