f785f9cb283eb79cdc70ad6a700583ea7207b6de
Thai Quoc Nguyen <thai_ng@outlook.com>
2017-03-26 04:56:55 -0700

    Highlight keywords Clickable keywords Flyout for keyword search

--- Client/TLDR-Client/CameraPage.xaml.cs
+++ Client/TLDR-Client/CameraPage.xaml.cs
@@ -1,11 +1,18 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
 using Windows.Graphics.Imaging;
 using Windows.Media.Capture;
 using Windows.Media.MediaProperties;
 using Windows.Media.Ocr;
+using Windows.Storage.Streams;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Documents;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
@@ -21,6 +28,7 @@ namespace TLDR_Client
     public sealed partial class CameraPage : Page
     {
         MediaCapture mediaCapture;
+        Dictionary<string, KeyPhrase> keyPhrases;
 
         public CameraPage()
         {
@@ -49,67 +57,134 @@ namespace TLDR_Client
 
         private async void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
-            var lowLagCapture = await mediaCapture.PrepareLowLagPhotoCaptureAsync(ImageEncodingProperties.CreateUncompressed(MediaPixelFormat.Bgra8));
-            var frame = await lowLagCapture.CaptureAsync();
-            await lowLagCapture.FinishAsync();
-            await mediaCapture.StopPreviewAsync();
+            try
+            {
 
-            ProgressBar.Visibility = Visibility.Visible;
-            SummarizedText.Visibility = Visibility.Visible;
-            SummarizedTextBlock.Visibility = Visibility.Visible;
-            SummarizedText.IsEnabled = false;
+                CapturedPhoto frame = await CapturePreviewFrame();
 
-            var ocrEngine = OcrEngine.TryCreateFromUserProfileLanguages();
-            var ocrResult = await ocrEngine.RecognizeAsync(frame.Frame.SoftwareBitmap);
+                await mediaCapture.StopPreviewAsync();
 
-            var imageSource = new SoftwareBitmapSource();
-            var bitmap = SoftwareBitmap.Convert(frame.Frame.SoftwareBitmap, BitmapPixelFormat.Bgra8, BitmapAlphaMode.Premultiplied);
-            await imageSource.SetBitmapAsync(bitmap);
+                EnableResultPane();
+
+                var ocrEngine = OcrEngine.TryCreateFromUserProfileLanguages();
+                var ocrResult = await ocrEngine.RecognizeAsync(frame.Frame.SoftwareBitmap);
+
+                await ShowResultImage(frame);
+                var client = new HttpClient();
+                var requestObject = new RequestObject() { content = ocrResult.Text };
+
+                var requestObjectString = JsonConvert.SerializeObject(requestObject);
+
+                var dataWriter = new DataWriter();
+                dataWriter.UnicodeEncoding = Windows.Storage.Streams.UnicodeEncoding.Utf8;
+                dataWriter.WriteString(requestObjectString);
+                var buffer = dataWriter.DetachBuffer();
+
+                var messageContent = new HttpBufferContent(buffer);
+
+                messageContent.Headers.ContentType = new Windows.Web.Http.Headers.HttpMediaTypeHeaderValue("application/json");
+                var message = new HttpRequestMessage(HttpMethod.Post, ConfigConstants.EndPoint)
+                {
+                    Content = messageContent
+                };
+
+                var response = await client.SendRequestAsync(message);
+                var responseBuffer = await response.Content.ReadAsBufferAsync();
+                var dataReader = DataReader.FromBuffer(responseBuffer);
+                dataReader.UnicodeEncoding = Windows.Storage.Streams.UnicodeEncoding.Utf8;
+                var responseString = dataReader.ReadString(responseBuffer.Length);
+                var responseObject = JsonConvert.DeserializeObject<ResponseObject>(responseString);
+
+                var summaryString = responseObject.summary;
+                var phrases = responseObject.key_phrases.Select(phrase => phrase.phrase).ToArray();
+                var stringParts = summaryString.Split(phrases, StringSplitOptions.RemoveEmptyEntries);
+                keyPhrases = responseObject.key_phrases.ToDictionary(phrase => phrase.phrase);
+
+                var keyPhraseIndices = phrases.Select(phrase => summaryString.IndexOf(phrase)).OrderBy(index => index).ToArray();
+                var paragraph = new Paragraph();
+
+                var normalStringStartIndex = 0;
+                for (var i = 0; i < phrases.Length; ++i)
+                {
+                    if (normalStringStartIndex < keyPhraseIndices[i])
+                    {
+                        paragraph.Inlines.Add(new Run() { Text = summaryString.Substring(normalStringStartIndex, keyPhraseIndices[i] - normalStringStartIndex) });
+                    }
+
+                    var uiContainter = new InlineUIContainer();
+                    var button = new Button() { Content = phrases[i], Background = new SolidColorBrush(Colors.Yellow) };
+                    button.Margin = new Thickness(0, 0, 0, 0);
+                    button.Padding = new Thickness(0, 0, 0, 0);
+
+                    button.Click += KeyPhraseClick;
+                    uiContainter.Child = button;
+                    paragraph.Inlines.Add(uiContainter);
+
+                    normalStringStartIndex = keyPhraseIndices[i] + phrases[i].Length;
+                }
+
+                if (normalStringStartIndex < summaryString.Length - 1)
+                {
+                    paragraph.Inlines.Add(new Run() { Text = summaryString.Substring(normalStringStartIndex) });
+                }
+
+                SummarizedText.Blocks.Add(paragraph);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            finally
+            {
+                ProgressBar.Visibility = Visibility.Collapsed;
+            }
+
+        }
+
+        private async void KeyPhraseClick(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            var phrase = button.Content as string;
+            var keyPhrase = keyPhrases[phrase];
+            var dialog = new Flyout()
+            {
+                Content = new WebView() { Source = keyPhrase.url, MaxHeight = RootGrid.ActualHeight, MaxWidth = RootGrid.ActualWidth },
+                Placement = Windows.UI.Xaml.Controls.Primitives.FlyoutPlacementMode.Full
+            };
+            button.Flyout = dialog;
+            button.Flyout.ShowAt(button);
+        }
+
+        private async System.Threading.Tasks.Task ShowResultImage(CapturedPhoto frame)
+        {
+            SoftwareBitmapSource imageSource = await GetImageSourceFromCapturedFrame(frame);
 
             ResultImage.Source = imageSource;
             ResultImage.Visibility = Visibility.Visible;
             PreviewElement.Visibility = Visibility.Collapsed;
-            
-            var textAngle = -ocrResult.TextAngle.Value;
+        }
 
-            foreach (var line in ocrResult.Lines)
-            {
-                foreach (var word in line.Words)
-                {
-                    var textBlock = new TextBlock();
+        private static async System.Threading.Tasks.Task<SoftwareBitmapSource> GetImageSourceFromCapturedFrame(CapturedPhoto frame)
+        {
+            var imageSource = new SoftwareBitmapSource();
+            var bitmap = SoftwareBitmap.Convert(frame.Frame.SoftwareBitmap, BitmapPixelFormat.Bgra8, BitmapAlphaMode.Premultiplied);
+            await imageSource.SetBitmapAsync(bitmap);
+            return imageSource;
+        }
 
-                    var x = word.BoundingRect.X;
-                    var y = word.BoundingRect.Y;
-                    textBlock.Margin = new Thickness() { Left = x, Top = y};
-                    ResultGrid.Children.Add(textBlock);
-                }
-            }
+        private async System.Threading.Tasks.Task<CapturedPhoto> CapturePreviewFrame()
+        {
+            var lowLagCapture = await mediaCapture.PrepareLowLagPhotoCaptureAsync(ImageEncodingProperties.CreateUncompressed(MediaPixelFormat.Bgra8));
+            var frame = await lowLagCapture.CaptureAsync();
+            await lowLagCapture.FinishAsync();
+            return frame;
+        }
 
-            ResultGrid.RenderTransform = new RotateTransform() { CenterX = ResultImage.Width / 2, CenterY = ResultImage.Height / 2, Angle = ocrResult.TextAngle.Value };
-
-
+        private void EnableResultPane()
+        {
             ProgressBar.Visibility = Visibility.Visible;
-            SummarizedText.IsEnabled = false;
-
-            // Do the thing
-            var client = new HttpClient();
-            var requestObject = new RequestObject() { content = ocrResult.Text };
-            var requestObjectString = JsonConvert.SerializeObject(requestObject);
-            var messageContent = new HttpStringContent(requestObjectString);
-            messageContent.Headers.ContentType = new Windows.Web.Http.Headers.HttpMediaTypeHeaderValue("application/json");
-            var message = new HttpRequestMessage(HttpMethod.Post, ConfigConstants.EndPoint)
-            {
-                Content = messageContent
-            };
-
-            var response = await client.SendRequestAsync(message);
-
-            var responseString = await response.Content.ReadAsStringAsync();
-            var responseObject = JsonConvert.DeserializeObject<ResponseObject>(responseString);
-
-            SummarizedText.Text = responseObject.summary;
-            SummarizedText.IsEnabled = true;
-            ProgressBar.Visibility = Visibility.Collapsed;
+            SummarizedText.Visibility = Visibility.Visible;
+            SummarizedTextBlock.Visibility = Visibility.Visible;
         }
     }
 }
--- Client/TLDR-Client/TextPage.xaml.cs
+++ Client/TLDR-Client/TextPage.xaml.cs
@@ -3,6 +3,13 @@ using Windows.UI.Xaml.Controls;
 using Windows.Web.Http;
 using System;
 using Newtonsoft.Json;
+using Windows.Storage.Streams;
+using System.Linq;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Media;
+using Windows.UI;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -13,6 +20,7 @@ namespace TLDR_Client
     /// </summary>
     public sealed partial class TextPage : Page
     {
+        Dictionary<string, KeyPhrase> keyPhrases;
         public TextPage()
         {
             this.InitializeComponent();
@@ -20,28 +28,96 @@ namespace TLDR_Client
 
         private async void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
-            ProgressBar.Visibility = Visibility.Visible;
-            SummarizedText.IsEnabled = false;
-
-            // Do the thing
-            var client = new HttpClient();
-            var requestObject = new RequestObject() { content = OriginalText.Text };
-            var requestObjectString = JsonConvert.SerializeObject(requestObject);
-            var messageContent = new HttpStringContent(requestObjectString);
-            messageContent.Headers.ContentType = new Windows.Web.Http.Headers.HttpMediaTypeHeaderValue("application/json");
-            var message = new HttpRequestMessage(HttpMethod.Post, ConfigConstants.EndPoint)
+            try
             {
-                Content = messageContent
-            };
+                ProgressBar.Visibility = Visibility.Visible;
 
-            var response = await client.SendRequestAsync(message);
+                var client = new HttpClient();
+                var requestObject = new RequestObject() { content = OriginalText.Text };
 
-            var responseString = await response.Content.ReadAsStringAsync();
-            var responseObject = JsonConvert.DeserializeObject<ResponseObject>(responseString);
+                var requestObjectString = JsonConvert.SerializeObject(requestObject);
 
-            SummarizedText.Text = responseObject.summary;
-            SummarizedText.IsEnabled = true;
-            ProgressBar.Visibility = Visibility.Collapsed;
+                var dataWriter = new DataWriter();
+                dataWriter.UnicodeEncoding = Windows.Storage.Streams.UnicodeEncoding.Utf8;
+                dataWriter.WriteString(requestObjectString);
+                var buffer = dataWriter.DetachBuffer();
+
+                var messageContent = new HttpBufferContent(buffer);
+
+                messageContent.Headers.ContentType = new Windows.Web.Http.Headers.HttpMediaTypeHeaderValue("application/json");
+                var message = new HttpRequestMessage(HttpMethod.Post, ConfigConstants.EndPoint)
+                {
+                    Content = messageContent
+                };
+
+                var response = await client.SendRequestAsync(message);
+                var responseBuffer = await response.Content.ReadAsBufferAsync();
+                var dataReader = DataReader.FromBuffer(responseBuffer);
+                dataReader.UnicodeEncoding = Windows.Storage.Streams.UnicodeEncoding.Utf8;
+                var responseString = dataReader.ReadString(responseBuffer.Length);
+                var responseObject = JsonConvert.DeserializeObject<ResponseObject>(responseString);
+
+                var summaryString = responseObject.summary;
+                var phrases = responseObject.key_phrases.Select(phrase => phrase.phrase).ToArray();
+                var stringParts = summaryString.Split(phrases, StringSplitOptions.RemoveEmptyEntries);
+                keyPhrases = responseObject.key_phrases.ToDictionary(phrase => phrase.phrase);
+
+                var keyPhraseIndices = phrases.Select(phrase => summaryString.IndexOf(phrase)).ToArray();
+
+                var paragraph = new Paragraph();
+
+                var normalStringStartIndex = 0;
+                for (var i = 0; i < phrases.Length; ++i)
+                {
+                    if (normalStringStartIndex < keyPhraseIndices[i])
+                    {
+                        paragraph.Inlines.Add(new Run() { Text = summaryString.Substring(normalStringStartIndex, keyPhraseIndices[i] - normalStringStartIndex) });
+                    }
+
+                    var uiContainter = new InlineUIContainer();
+                    var button = new Button() { Content = phrases[i], Background = new SolidColorBrush(Colors.Yellow) };
+                    button.Margin = new Thickness(0, 0, 0, 0);
+                    button.Padding = new Thickness(0, 0, 0, 0);
+
+                    button.Click += KeyPhraseClick;
+                    uiContainter.Child = button;
+                    paragraph.Inlines.Add(uiContainter);
+
+                    normalStringStartIndex = keyPhraseIndices[i] + phrases[i].Length;
+                }
+
+                if (normalStringStartIndex < summaryString.Length - 1)
+                {
+                    paragraph.Inlines.Add(new Run() { Text = summaryString.Substring(normalStringStartIndex) });
+                }
+
+                SummarizedText.Blocks.Add(paragraph);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            finally
+            {
+                ProgressBar.Visibility = Visibility.Collapsed;
+            }
+
         }
+
+
+        private void KeyPhraseClick(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            var phrase = button.Content as string;
+            var keyPhrase = keyPhrases[phrase];
+            var dialog = new Flyout()
+            {
+                Content = new WebView() { Source = keyPhrase.url, MaxHeight=RootGrid.ActualHeight, MaxWidth=RootGrid.ActualWidth },
+                Placement = Windows.UI.Xaml.Controls.Primitives.FlyoutPlacementMode.Full
+            };
+            button.Flyout = dialog;
+            button.Flyout.ShowAt(button);
+        }
+
     }
 }
