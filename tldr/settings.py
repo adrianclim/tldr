596176994b323b7da1d5454acff7c11e94013a21
Thai Nguyen <qtoncoding@users.noreply.github.com>
2017-03-26 08:55:08 -0700

    Update settings.py

--- tldr/settings.py
+++ tldr/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.statics',
     'rest_framework',
     'rest_framework_docs',
 ]
