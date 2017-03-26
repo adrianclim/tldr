0028f9ab494ce84d4fab0ed3fe38f5c3d6f7b6d1
Thai Quoc Nguyen <thai_ng@outlook.com>
2017-03-26 08:02:09 -0700

    requirement constant

--- Client/TLDR-Client/ConfigConstants.cs
+++ Client/TLDR-Client/ConfigConstants.cs
@@ -9,6 +9,6 @@ namespace TLDR_Client
     public class ConfigConstants
     {
         //public static Uri EndPoint = new Uri("http://159.203.15.127:8000/api/summarize");
-        public static Uri EndPoint = new Uri("http://tldr-summarize.azurewebsites.net:8000/api/summarize");
+        public static Uri EndPoint = new Uri("http://tldrsummarize.azurewebsites.net:8000/api/summarize");
     }
 }
