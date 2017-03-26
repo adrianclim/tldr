5a2f0b0e9c815df874850fb00087ce2e44f9eb89
adrianlim <aclim@sfu.ca>
2017-03-26 06:41:52 -0700

    initial commit of scraper

--- Server/tldr/summarizer/analysis.py
+++ Server/tldr/summarizer/analysis.py
@@ -2,6 +2,7 @@ from tldr import secret
 from urllib.request import Request, urlopen
 import json
 from summarizer import summary_tool
+from bs4 import BeautifulSoup as bs4
 
 
 AZURE_URL = "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases"
@@ -86,3 +87,24 @@ def extract_summary(text):
         return text
 
     return summary
+
+
+def scrape_url(url):
+    html = urlopen(url).read()
+    soup = bs4(html, "html.parser")
+
+    # kill all script and style elements
+    for script in soup(["script", "style"]):
+        script.extract()    # rip it out
+
+    # get text
+    text = soup.get_text()
+
+    # break into lines and remove leading and trailing space on each
+    lines = (line.strip() for line in text.splitlines())
+    # break multi-headlines into a line each
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    # drop blank lines
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+
+    return text
