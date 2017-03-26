1f0688d0546c3ccee8358991e3b8287280c6c601
Thai Quoc Nguyen <thai_ng@outlook.com>
2017-03-26 07:00:17 -0700

    Point to Azure

--- Client/TLDR-Client/ConfigConstants.cs
+++ Client/TLDR-Client/ConfigConstants.cs
@@ -8,6 +8,7 @@ namespace TLDR_Client
 {
     public class ConfigConstants
     {
-        public static Uri EndPoint = new Uri("http://159.203.15.127:8000/api/summarize");
+        //public static Uri EndPoint = new Uri("http://159.203.15.127:8000/api/summarize");
+        public static Uri EndPoint = new Uri("http://tldr-summarize.azurewebsites.net:8000/api/summarize");
     }
 }
