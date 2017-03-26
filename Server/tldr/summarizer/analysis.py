2de69ae6a89b781e56b177f66c904d82c7f80bca
adrianlim <aclim@sfu.ca>
2017-03-26 01:07:58 -0700

    Add bing stuff

--- Server/tldr/summarizer/analysis.py
+++ Server/tldr/summarizer/analysis.py
@@ -5,8 +5,10 @@ from summarizer import summary_tool
 
 
 AZURE_URL = "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases"
+BING_URL = "https://api.cognitive.microsoft.com/bing/v5.0/search"
 
 headers = {'Content-Type': 'application/json', 'Ocp-Apim-Subscription-Key': secret.AZURE_SUB_KEY}
+bing_headers = {'Content-Type': 'application/json', 'Ocp-Apim-Subscription-Key': secret.BING_SUB_KEY}
 
 
 def extract_key_phrases(text):
@@ -22,6 +24,18 @@ def extract_key_phrases(text):
     obj = json.loads(result)['documents'][0]['keyPhrases']
 
     return obj
+
+
+def search_key_phrase(key_phrase):
+    search_url = BING_URL + "?q="
+    complete_url = search_url + "+".join(key_phrase.split(" ")) + "&count=1"
+    request = Request(complete_url,
+                      headers=bing_headers)
+    response = urlopen(request)
+    result = response.read()
+    obj = json.loads(result)
+    return {'short': obj['webPages']['value'][0]['name'],
+            'url': obj['webPages']['value'][0]['url']}
 
 
 def extract_summary(title, text):
