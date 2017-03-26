2e04bc33cb9b834ea04daa2edcd410ba21d4473b
adrianlim <aclim@sfu.ca>
2017-03-26 06:55:15 -0700

    Parse paragraphs

--- Server/tldr/summarizer/analysis.py
+++ Server/tldr/summarizer/analysis.py
@@ -92,19 +92,6 @@ def extract_summary(text):
 def scrape_url(url):
     html = urlopen(url).read()
     soup = bs4(html, "html.parser")
+    texts = soup.find('p').getText()
 
-    # kill all script and style elements
-    for script in soup(["script", "style"]):
-        script.extract()    # rip it out
-
-    # get text
-    text = soup.get_text()
-
-    # break into lines and remove leading and trailing space on each
-    lines = (line.strip() for line in text.splitlines())
-    # break multi-headlines into a line each
-    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-    # drop blank lines
-    text = '\n'.join(chunk for chunk in chunks if chunk)
-
-    return text
+    return texts
