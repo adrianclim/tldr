7fa6f8348339dd21b61c9d13724b46fb637d0be0
Thai Quoc Nguyen <thai_ng@outlook.com>
2017-03-29 12:35:35 -0700

    misc updates

--- Client/TLDR-Client/ConfigConstants.cs
+++ Client/TLDR-Client/ConfigConstants.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Storage.Streams;
 
 namespace TLDR_Client
 {
     public class ConfigConstants
     {
-        public static Uri EndPoint = new Uri("http://159.203.15.127:8000/api/summarize");
-        //public static Uri EndPoint = new Uri("http://tldrsummarize.azurewebsites.net:80/api/summarize");
+        //public static Uri EndPoint = new Uri("http://159.203.15.127:8000/api/summarize");
+        public static Uri EndPoint = new Uri("http://localhost:8000/api/summarize");
     }
 }
